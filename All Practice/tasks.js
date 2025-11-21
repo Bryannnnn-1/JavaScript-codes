f5310eaf4790f54d1659a4822be41ccaea820bf6
Bryan Uzoma <bryan.uzoma.06@gmail.com>
2025-11-21 09:38:44 +0000

    Updated

---
+++ All Practice/tasks.js
@@ -0,0 +1,11 @@
+export const tasks = [{
+        id:1, 
+        title:"Learn Node.js", 
+        completed:false
+    },
+    {
+        id:2, 
+        title:"Build an API", 
+        completed:false
+    }
+];
